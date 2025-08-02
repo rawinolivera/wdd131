8deb10889cae2cb40bfb92301eeb07bb90d2a26f
rawinolivera <oli18001@byui.edu>
2025-08-02 20:15:14 -0300

    verifiying requiements W3

--- scripts/place.js
+++ scripts/place.js
@@ -2,3 +2,20 @@ var date = new Date();
 var year = date.getFullYear();
 document.querySelector('#current-year').textContent = year;
 document.querySelector('#last-mod').textContent = document.lastModified; 
+
+
+const temp = 10;
+const windSpeed = 14;
+document.querySelector('#temp').textContent = `${temp}`;
+document.querySelector('#wind').textContent = `${windSpeed}`;
+
+function calculateWindChill(temp, windSpeed) {
+  if(temp <= 10 && windSpeed >= 4.8){
+    const windChill = 13.12 + 0.6215 * temp - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temp * Math.pow(windSpeed, 0.16);
+    return `${windChill.toFixed(1)} °C`;
+  }else{
+    return "N/A";
+  }
+
+}
+document.querySelector('#w-chill').textContent = calculateWindChill(temp, windSpeed);
