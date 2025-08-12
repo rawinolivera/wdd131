4095486722e99cb11f42da21685ab426ef401edd
rawinolivera <oli18001@byui.edu>
2025-08-12 03:03:28 -0300

    W05 Assignment: Product Review Form

---
+++ scripts/form.js
@@ -0,0 +1,47 @@
+var date = new Date();
+var year = date.getFullYear();
+document.querySelector('#current-year').textContent = year;
+document.querySelector('#last-mod').textContent = document.lastModified;
+
+const products = [
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
+];
+
+const product = document.querySelector("#product");
+
+function createOption (){
+  products.forEach(item => {
+    let option = document.createElement("option");
+
+    option.textContent = `${item.name}`
+    option.id = `${item.id}`
+
+    product.appendChild(option);
+  })
+}
+
+createOption();
